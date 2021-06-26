f3292da5fc3af410f65e862b1cfe2d7947b5edac
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-26 16:10:04 -0400

    created the forms for email password and password confirmation and the events onchange and onsubmit to grab the values of the input once the register button is submitted

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,7 +1,66 @@
 import React, { Component } from 'react';
 
 export default class Registration extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: '',
+      registrationErrors: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlChange = this.handlChange.bind(this);
+  }
+
+  handlChange(event) {
+    // console.log('handle change', event);
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    console.log('form submitted');
+    console.log(event);
+    event.preventDefault();
+  }
   render() {
-    return <div>Registration goes here</div>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handlChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handlChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password_confirmation"
+            placeholder="Confirm Password"
+            value={this.state.password_confirmation}
+            onChange={this.handlChange}
+            required
+          />
+
+          <button tupe="submit">Register</button>
+        </form>
+      </div>
+    );
   }
 }
