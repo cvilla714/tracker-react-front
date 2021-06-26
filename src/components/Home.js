6ebf6c39349cb8b604d030866c2f1837297a650e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-26 17:29:52 -0400

    changed it to class component and  added the stauts and passed the prop for checking if a user was logged in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Registration from './auth/Registration';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <Registration />
-    </div>
-  );
-};
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <h1>Status: {this.props.loggedInStatus}</h1>
+        <Registration />
+      </div>
+    );
+  }
+}
 
 export default Home;
