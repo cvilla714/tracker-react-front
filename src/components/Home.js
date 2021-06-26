19618f3fd7f68f65c5e79ec58183250f01888910
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-26 15:31:31 -0400

    imported registration component and called it inside the home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Registration from './auth/Registration';
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
       <h1>Home</h1>
+      <Registration />
     </div>
   );
 };
