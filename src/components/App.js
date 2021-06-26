052b740b85a6f13e9fa8c9c7f8b8489ee9943f15
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-26 18:13:58 -0400

    added the handlelogin to change the status to Logged in once a user register

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,14 @@ class App extends Component {
       loggedInStatus: 'NOT_LOGGED_IN',
       user: {},
     };
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(data) {
+    this.setState({
+      loggedInStatus: 'LOGGED_IN',
+      user: data.user,
+    });
   }
   render() {
     return (
@@ -22,7 +30,11 @@ class App extends Component {
               exact
               path={'/'}
               render={(props) => (
-                <Home {...props} loggedInStatus={this.state.loggedInStatus} />
+                <Home
+                  {...props}
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.loggedInStatus}
+                />
               )}
             />
             <Route
