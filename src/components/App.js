6059eee2d66dae6d0f2af1ad3456b917cbafa0b7
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-26 15:31:07 -0400

    deleted the logo and counter component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
-import { Counter } from '../features/counter/Counter';
 import './App.css';
 import Home from './Home';
 import Dashboard from './Dashboard';
