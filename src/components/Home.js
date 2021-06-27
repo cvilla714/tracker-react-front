f0bf781dccd10584d5537755dcad8c3473cbd1a4
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-27 02:51:32 -0400

    added the request using axios to delete the session of the user that was logged in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Registration from './auth/Registration';
 import Login from './auth/Login';
 
@@ -16,6 +17,14 @@ class Home extends Component {
   }
 
   handleLogoutClick() {
+    axios
+      .delete('http://localhost:3001/logout', { withCredentials: true })
+      .then((response) => {
+        this.props.handleLogout();
+      })
+      .catch((error) => {
+        console.log('logout error', error);
+      });
     this.props.handleLogout();
   }
   render() {
@@ -23,7 +32,7 @@ class Home extends Component {
       <div>
         <h1>Home</h1>
         <h1>Status: {this.props.loggedInStatus}</h1>
-        <button onClick={() => this.handleLogoutClick()}></button>
+        <button onClick={() => this.handleLogoutClick()}>Logout</button>
         <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
         <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
       </div>
