e4c213b153bed25ba691d22bbd562f1c7c559755
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-29 00:58:45 -0400

    converted to a functional component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 import Registration from './auth/Registration';
 import Login from './auth/Login';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-  }
-
-  handleSuccessfulAuth(data) {
+const Home = (props) => {
+  const handleSuccessfulAuth = (data) => {
     console.log(data.user);
-    this.props.handleLogin(data);
-    this.props.history.push('/dashboard');
-  }
+    props.handleLogin(data);
+    props.history.push('/dashboard');
+  };
 
-  handleLogoutClick() {
+  const handleLogoutClick = () => {
     axios
       .delete('http://localhost:3001/logout', { withCredentials: true })
       .then((response) => {
-        this.props.handleLogout();
+        props.handleLogout();
       })
       .catch((error) => {
         console.log('logout error', error);
       });
-    this.props.handleLogout();
-  }
-  render() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <h1>Status: {this.props.loggedInStatus}</h1>
-        {/* <h1>Status: {this.props.user}</h1> */}
-        <h2>User : {this.props.user}</h2>
-        <button onClick={() => this.handleLogoutClick()}>Logout</button>
-        <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
-        <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
-      </div>
-    );
-  }
-}
+    props.handleLogout();
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <h1>Status: {props.loggedInStatus}</h1>
+      {/* <h1>Status: {this.props.user}</h1> */}
+      <h2>User : {props.user}</h2>
+      <button onClick={() => handleLogoutClick()}>Logout</button>
+      <Registration handleSuccessfulAuth={handleSuccessfulAuth} />
+      <Login handleSuccessfulAuth={handleSuccessfulAuth} />
+    </div>
+  );
+};
 
 export default Home;
