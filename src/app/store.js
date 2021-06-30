bf5b23ca20eb81e039672b39feb18aecbf07e2b9
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-30 14:15:17 -0400

    added the registration reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+
+import registrationReducer from '../features/user/registrationSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    registration: registrationReducer,
   },
 });
