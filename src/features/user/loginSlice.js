512031f40768615dec1a7c3e7f68df9661985ebc
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-01 15:12:13 -0400

    created the loginUser to handle the axios post request using createAsyncThunk

--- src/features/user/loginSlice.js
+++ src/features/user/loginSlice.js
@@ -1,4 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const loginUser = createAsyncThunk(
+  'login/loginUser',
+  async (args, { getState }) => {
+    const {
+      login: {
+        user: { email, password },
+      },
+    } = getState();
+    return axios
+      .post(
+        'http://localhost:3001/sessions',
+        {
+          user: {
+            email,
+            password,
+          },
+        },
+        { withCredentials: true },
+      )
+      .then((response) => {
+        if (response.data.status === 'created') {
+          return response.data;
+        }
+      });
+  },
+);
 
 const initialState = {
   user: {
@@ -18,8 +46,24 @@ const loginSlice = createSlice({
       state.user[name] = value;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.loading = false;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.error = action.error.message;
+        state.loading = false;
+      });
+  },
 });
 
 export const { setUserProperty } = loginSlice.actions;
 
 export default loginSlice.reducer;
+
+export const selectIsLoginLoading = (state) => state.login.loading;
