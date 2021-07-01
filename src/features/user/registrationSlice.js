e1fbb0f0f2c7069dfb5ecdc5ba7c1af51225c2e5
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-01 13:21:35 -0400

    passed the user information from the from to sent it with the post request and create the new user,also added user to state inside setUser

--- src/features/user/registrationSlice.js
+++ src/features/user/registrationSlice.js
@@ -1,44 +1,53 @@
-import axios from "axios";
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const createUser = createAsyncThunk(
-  "registration/createUser",
+  'registration/createUser',
   // You either pass in the arg to use, or you pull them out of the state.
   async (userFromArg, { getState }) => {
-    const user = getState().registration.user;
+    // const user = getState().registration.user;
+    const {
+      registration: {
+        user: { email, password, password_confirmation },
+      },
+    } = getState();
     return axios
       .post(
-        "http://localhost:3001/registrations",
+        'http://localhost:3001/registrations',
         {
-          user,
+          user: {
+            email,
+            password,
+            password_confirmation,
+          },
         },
-        { withCredentials: true }
+        { withCredentials: true },
       )
       .then((response) => {
-        if (response.data.status === "created") {
+        if (response.data.status === 'created') {
           return response.data;
         }
       });
-  }
+  },
 );
 
 const initialState = {
   user: {
-    email: "",
-    password: "",
-    password_confirmation: "",
+    email: '',
+    password: '',
+    password_confirmation: '',
   },
   loading: false,
   error: null,
 };
 
 const registrationSlice = createSlice({
-  name: "registration",
+  name: 'registration',
   initialState,
   reducers: {
     setUsers: (state, action) => {
       const { email, password, password_confirmation } = action.payload;
-      state = {
+      state.user = {
         email,
         password,
         password_confirmation,
@@ -46,18 +55,19 @@ const registrationSlice = createSlice({
     },
     setUserProperty: (state, action) => {
       const { name, value } = action.payload;
-      state[name] = value;
+      state.user[name] = value;
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(createUser.pending, (state, action) => {
-      state.loading = true;
-    }),
-      builder.addCase(createUser.fulfilled, (state, action) => {
+    builder
+      .addCase(createUser.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
         state.user = action.payload;
         state.loading = false;
-      }),
-      builder.addCase(createUser.rejected, (state, action) => {
+      })
+      .addCase(createUser.rejected, (state, action) => {
         state.error = action.error.message;
         state.loading = false;
       });
@@ -68,4 +78,5 @@ export const { setUsers, setUserProperty } = registrationSlice.actions;
 
 export default registrationSlice.reducer;
 
-export const selectIsRegistrationLoading = state.registration.loading;
+export const selectIsRegistrationLoading = (state) =>
+  state.registration.loading;
