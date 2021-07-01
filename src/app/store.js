876b2e3d58ad1c2bd28df745382c1591f9661a3e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-01 03:01:46 -0400

    imported the login reducer and added it to the reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import registrationReducer from '../features/user/registrationSlice';
-
+import loginReducer from '../features/user/loginSlice.js';
 export const store = configureStore({
   reducer: {
     registration: registrationReducer,
+    login: loginReducer,
   },
 });
