19eaf5c3dcdf65e64a7991655d37f2d693f7ca72
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-02 16:23:40 -0400

    extracted Navbar, links ,registration and login components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import Registration from '../containers/auth/Registration';
-import Login from '../containers/auth/Login';
 import ExpenseItem from './ExpenseItem';
-import { Navbar, Nav } from 'react-bootstrap';
 
 const Home = (props) => {
   const handleSuccessfulAuth = (data) => {
@@ -33,33 +29,13 @@ const Home = (props) => {
 
   return (
     <div>
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="#home">Expenses Tracker</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Link to="/" className="text-light">
-            Home
-          </Link>
-          <Link to="/register" className="text-white mx-2">
-            Register
-          </Link>
-          <Link to="/login" className="text-light">
-            Login
-          </Link>
-        </Nav>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Hello: <a href="#login">Guest</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Navbar>
       <h1>Home</h1>
       <h1>Status: {props.loggedInStatus}</h1>
       {/* <h1>Status: {this.props.user}</h1> */}
       <h2>User : {props.user}</h2>
       <button onClick={() => handleLogoutClick()}>Logout</button>
-      <Registration handleSuccessfulAuth={handleSuccessfulAuth} />
-      <Login handleSuccessfulAuth={handleSuccessfulAuth} />
+      {/* <Registration handleSuccessfulAuth={handleSuccessfulAuth} />
+      <Login handleSuccessfulAuth={handleSuccessfulAuth} /> */}
       <ExpenseItem />
     </div>
   );
