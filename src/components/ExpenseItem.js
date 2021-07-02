1acd7513a7e723ca2c09d4193e9cf034eac1b47f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-02 11:59:20 -0400

    added variables for each piece of data for expenses, data, and amount

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Expenseitem.css';
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 7, 1);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 295;
   return (
     <div className="expense-item">
-      <div>Thursday July 1st 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$295</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
