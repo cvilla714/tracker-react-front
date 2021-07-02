c438d8671520844f50bd8a80fe61e5fd26fe5983
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-02 16:24:30 -0400

    added the style to form

--- src/containers/auth/Login.js
+++ src/containers/auth/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   loginUser,
@@ -27,8 +28,38 @@ const Login = (props) => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="container">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={handlChange}
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlChange}
+            required
+          />
+        </Form.Group>
+        <Button type="submit" disabled={isLoginLoading}>
+          {isLoginLoading ? 'Loggin you in...' : 'Log in'}{' '}
+        </Button>{' '}
+      </Form>
+      {/* <form onSubmit={handleSubmit}>
         <input
           type="email"
           name="email"
@@ -37,7 +68,6 @@ const Login = (props) => {
           onChange={handlChange}
           required
         />
-
         <input
           type="password"
           name="password"
@@ -46,11 +76,10 @@ const Login = (props) => {
           onChange={handlChange}
           required
         />
-
         <button type="submit" disabled={isLoginLoading}>
           {isLoginLoading ? 'Loggin you in...' : 'Log in'}{' '}
-        </button>
-      </form>
+        </button>{' '}
+      </form> */}
     </div>
   );
 };
