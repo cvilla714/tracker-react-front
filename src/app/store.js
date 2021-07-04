5399093c0735c9f5b441e6548eb37d627bc00f2a
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-03 20:06:57 -0400

    aded new reducer checkLoginStatus

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import registrationReducer from '../features/user/registrationSlice';
 import loginReducer from '../features/user/loginSlice';
+import { checkLoginStatus } from '../features/user/statusSlice';
+// import usersReducer from '../features/user/statusSlice';
 
 export const store = configureStore({
   reducer: {
     registration: registrationReducer,
     login: loginReducer,
+    // users: usersReducer,
+    [checkLoginStatus.reducerPath]: checkLoginStatus.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(checkLoginStatus.middleware);
   },
 });
