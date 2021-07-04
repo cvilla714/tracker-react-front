dc9a60dd0036fae25327df1420cbcbf7e1c2eed1
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-03 20:09:10 -0400

    added the logout funtion that makes the delete request to logout endpoint

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-const NavigationBar = () => {
+import axios from 'axios';
+// import { useGetLoginUserInfo } from '../features/user/statusSlice';
+
+const NavigationBar = (props) => {
+  const handleLogoutClick = () => {
+    axios
+      .delete('http://localhost:3001/logout', { withCredentials: true })
+      .then((response) => {
+        props.handleLogout();
+      })
+      .catch((error) => {
+        console.log('logout error', error);
+      });
+    props.handleLogout();
+  };
+  // const { data, error, isLoading } = useGetLoginUserInfo();
   return (
     <div>
       <Navbar bg="primary" variant="dark">
@@ -16,10 +31,21 @@ const NavigationBar = () => {
           <Link to="/login" className="text-light">
             Login
           </Link>
+          <li>
+            <a
+              href="?"
+              className="text-light mx-2"
+              // onClick={(e) => handleLogout(e)}
+              onClick={() => handleLogoutClick()}
+            >
+              {' '}
+              Logout
+            </a>
+          </li>
         </Nav>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>Hello: Guest</Navbar.Text>
+          <Navbar.Text>Hello: {props.useremail}</Navbar.Text>
         </Navbar.Collapse>
       </Navbar>
     </div>
