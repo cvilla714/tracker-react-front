09be706f0b1788fa6ea0f2de153c53cfc82e9899
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-04 21:34:12 -0400

    passed in the props from home components and created the month day and year function to render it to the browser

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './Expenseitem.css';
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 7, 1);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 295;
+const ExpenseItem = (props) => {
+  // const expenseDate = new Date(2021, 7, 1);
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = 295;
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
   return (
+    // <div className="expense-item">
+    //   <div>{expenseDate.toISOString()}</div>
+    //   <div className="expense-item__description">
+    //     <h2>{expenseTitle}</h2>
+    //     <div className="expense-item__price">{expenseAmount}</div>
+    //   </div>
+    // </div>
+
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      {/* <div>{props.date.toISOString()}</div> */}
+      {/* <div>{props.date.toLocaleString('en-US', { month: 'long' })}</div> */}
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
