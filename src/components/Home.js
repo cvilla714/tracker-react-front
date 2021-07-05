17e43d66b829f6593016c1374f1be01dbb891bfb
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-05 14:20:10 -0400

    cleaned up commented code and passed the correct prop name

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import ExpenseItem from './ExpenseItem';
 import Expenses from './Expenses';
 
 const Home = () => {
@@ -27,28 +26,7 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-
-      <Expenses expenses={expenses} />
-      {/* <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      /> */}
+      <Expenses items={expenses} />
     </div>
   );
 };
