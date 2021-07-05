664a357c8113c4fad55b1b9d0c0388908a76426f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-05 19:03:11 -0400

    moved it to root folder

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import NavigationBar from './Navbar';
-import Home from './Home';
+import Home from './Expenses/Home';
 import Dashboard from './Dashboard';
 import Registration from '../containers/auth/Registration';
 import Login from '../containers/auth/Login';
