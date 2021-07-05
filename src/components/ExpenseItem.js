f9885d85c4519491e5cfc2437eb297ab653de6be
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-05 14:00:41 -0400

    clean up the code from commented code and imported the ExpenseDate component to render the date correctly

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,28 +1,11 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import './Expenseitem.css';
 
 const ExpenseItem = (props) => {
-  // const expenseDate = new Date(2021, 7, 1);
-  // const expenseTitle = 'Car Insurance';
-  // const expenseAmount = 295;
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
   return (
-    // <div className="expense-item">
-    //   <div>{expenseDate.toISOString()}</div>
-    //   <div className="expense-item__description">
-    //     <h2>{expenseTitle}</h2>
-    //     <div className="expense-item__price">{expenseAmount}</div>
-    //   </div>
-    // </div>
-
     <div className="expense-item">
-      {/* <div>{props.date.toISOString()}</div> */}
-      {/* <div>{props.date.toLocaleString('en-US', { month: 'long' })}</div> */}
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
