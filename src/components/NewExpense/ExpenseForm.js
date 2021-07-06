1db0d29197d73e8ec35d1da8861d417692113110
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-06 10:33:30 -0400

    fixed closing brackets and logged to the console the values of the input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -18,53 +18,56 @@ const ExpenseForm = () => {
       ...userInput,
       enterTitle: e.target.value,
     });
-
-    const amountHandler = (e) => {
-      // setEnterAmount(e.target.value);
-      setUserInput({
-        ...userInput,
-        enterAmount: e.target.value,
-      });
-    };
-
-    const datetHandler = (e) => {
-      //   setEnterDate({
-      setUserInput({
-        ...userInput,
-        enterDate: e.target.value,
-      });
-    };
-    return (
-      <form action="">
-        <div className="new-expense__controls">
-          <div className="new-expense__control">
-            <label htmlFor="">Title</label>
-            <input type="text" onChange={titleChangeHandler} />
-          </div>
-          <div className="new-expense__control">
-            <label htmlFor="">Amount</label>
-            <input
-              type="number"
-              min="0.01"
-              step="0.01"
-              onChange={amountHandler}
-            />
-          </div>
-          <div className="new-expense__control">
-            <label htmlFor="">Date</label>
-            <input
-              type="date"
-              min="2019-01-01"
-              max="2022-12-31"
-              onChange={datetHandler}
-            />
-          </div>
-        </div>
-        <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
-        </div>
-      </form>
-    );
+    console.log(e.target.value);
   };
+
+  const amountHandler = (e) => {
+    // setEnterAmount(e.target.value);
+    setUserInput({
+      ...userInput,
+      enterAmount: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+
+  const datetHandler = (e) => {
+    //   setEnterDate({
+    setUserInput({
+      ...userInput,
+      enterDate: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+  return (
+    <form action="">
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label htmlFor="">Title</label>
+          <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="">Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="">Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={datetHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
 };
 export default ExpenseForm;
