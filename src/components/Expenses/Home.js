f0741c28dec17442aa05c2910eb174f84bedfb31
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-07 00:56:04 -0400

    extracted expenses as initial expenses and use inside the useState to render the next value provided in the form

--- src/components/Expenses/Home.js
+++ src/components/Expenses/Home.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Expenses from './Expenses';
 import NewExpense from '../NewExpense/NewExpense';
 
+const InitialExpenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 const Home = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(InitialExpenses);
 
   const addExpenseHandler = (expense) => {
-    console.log('In Home.js');
+    // console.log('In Home.js');
+    // console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+    // setExpenses([expense, ...expenses]);
     console.log(expense);
   };
   return (
