5d92c805e4fe33cdfa84ae75dd013ca7e909a10a
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-07 10:49:03 -0400

    added 3 solutons for rendering conditional components if there are no expenses for the selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,14 +8,25 @@ const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState('2021');
 
   const filterChangeHandler = (selectedyear) => {
-    // console.log('coming from Expenses.js');
-    // console.log(selectedyear);
     setFilterYear(selectedyear);
   };
 
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filterYear;
   });
+
+  let expensesContent = <p className="text-warning">No Expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
@@ -24,35 +35,32 @@ const Expenses = (props) => {
           onChangeFitler={filterChangeHandler}
         />
 
-        {/* {props.items.map((expense) => ( */}
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-        {/* <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        /> */}
+        {expensesContent}
+        {/* {filteredExpenses.length === 0 ? (
+          <p className="text-warning">No Expenses found</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )} */}
+
+        {/* {filteredExpenses.length === 0 && (
+          <p className="text-warning">No Expenses found</p>
+        )}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */}
       </Card>
     </div>
   );
