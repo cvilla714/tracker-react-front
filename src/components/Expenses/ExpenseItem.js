612714e4454a610202e7bb9ccaf64f3d0c432d32
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-07 00:54:54 -0400

    deleted the state to change the title, instead passed the props to the title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import './Expenseitem.css';
 import Card from '../Ui/Card';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle('Update');
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   setTitle('Update');
+  //   console.log(title);
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         {/* <h2>{props.title}</h2> */}
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
+      {/* <button onClick={clickHandler}>Change title</button> */}
     </Card>
   );
 };
