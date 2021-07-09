0991b1f4cf5ba550bc79abd2a1cd99f1d971bd68
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-09 18:17:57 -0400

    fixed the year date to display correctly usnig the filter

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -4,7 +4,7 @@ import './ExpenseDate.css';
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
+  const year = props.date.toLocaleString('en-US', { year: 'numeric' });
   return (
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
