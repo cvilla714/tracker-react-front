337c4746aa53e63ef63d8918e1e5f506559c329c
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-09 18:19:50 -0400

    used rtk query to pull the data from the custom hook and render the information coming from the API

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import './ExpensesList.css';
+import { useGetUserExpensesQuery } from '../../features/user/statusSlice';
 
 const ExpensesList = (props) => {
+  const { data = [] } = useGetUserExpensesQuery();
+  // const [getUserExpenses] = useGetUserExpensesQuery();
   if (props.items.length === 0) {
     return <h2 className="text-warning">No Expenes Found</h2>;
   }
 
   return (
     <ul className="expenses-list">
-      {props.items.map((expense) => (
+      {/* <button onClick={() => getUserExpenses()}>You List</button> */}
+      {/* {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
+        />
+      ))} */}
+
+      {data.map((item) => (
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
         />
       ))}
     </ul>
