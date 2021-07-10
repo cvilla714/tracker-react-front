734049d1524d2a5de36dabc8a48ddd467c40e900
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-10 19:41:47 -0400

    deleted ommenetd code and applied condition to render no expenes found if the data is empty coming from the api

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -6,22 +6,13 @@ import { useGetUserExpensesQuery } from '../../features/user/statusSlice';
 const ExpensesList = (props) => {
   const { data = [] } = useGetUserExpensesQuery();
   // const [getUserExpenses] = useGetUserExpensesQuery();
-  if (props.items.length === 0) {
+  if (data.length === 0) {
+    // if (props.items.length === 0) {
     return <h2 className="text-warning">No Expenes Found</h2>;
   }
 
   return (
     <ul className="expenses-list">
-      {/* <button onClick={() => getUserExpenses()}>You List</button> */}
-      {/* {props.items.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))} */}
-
       {data.map((item) => (
         <ExpenseItem
           key={item.id}
