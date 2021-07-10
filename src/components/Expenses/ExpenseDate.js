980de910d88d86679b58b6f69502257ddd8bd13b
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-10 14:32:33 -0400

    loggin the date to see the format coming from the api

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './ExpenseDate.css';
+// import { useGetUserExpensesQuery } from '../../features/user/statusSlice';
 
 const ExpenseDate = (props) => {
+  // const { data = [] } = useGetUserExpensesQuery();
+
+  // console.log({ data });
+
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.toLocaleString('en-US', { year: 'numeric' });
+  console.log(month);
   return (
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
