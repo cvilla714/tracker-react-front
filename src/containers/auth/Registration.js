4de8481715e726fb193806ec65942981a9c0cf03
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-12 01:03:54 -0400

    imported custom hook and extracted the registerUser query to hit the endpoint regsitration and register a new user

--- src/containers/auth/Registration.js
+++ src/containers/auth/Registration.js
@@ -1,32 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  createUser,
-  selectIsRegistrationLoading,
-  setUserProperty,
-} from '../../features/user/registrationSlice';
+// import { useDispatch, useSelector } from 'react-redux';
+// import {
+//   createUser,
+//   selectIsRegistrationLoading,
+//   setUserProperty,
+// } from '../../features/user/registrationSlice';
+import { useRegisterUserMutation } from '../../features/user/statusSlice';
 
 const Registration = (props) => {
-  const dispatch = useDispatch();
-  const isRegistering = useSelector(selectIsRegistrationLoading);
+  // const dispatch = useDispatch();
+  // const isRegistering = useSelector(selectIsRegistrationLoading);
 
-  const user = useSelector((state) => state);
+  // const user = useSelector((state) => state);
 
-  const { email, password, password_confirmation } = user;
+  // const { email, password, password_confirmation } = user;
 
-  const handleChange = (event) => {
-    dispatch(
-      setUserProperty({
-        name: event.target.name,
-        value: event.target.value,
-      }),
-    );
+  // const handleChange = (event) => {
+  //   dispatch(
+  //     setUserProperty({
+  //       name: event.target.name,
+  //       value: event.target.value,
+  //     }),
+  //   );
+  // };
+
+  const [registerUser] = useRegisterUserMutation();
+
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [userPasswordConfirmation, setUserPasswordCofirmation] = useState('');
+
+  const emailHandleChange = (e) => {
+    setUserEmail(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const passwordHandleChange = (e) => {
+    setUserPassword(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const passwordConfirmationHandleChange = (e) => {
+    setUserPasswordCofirmation(e.target.value);
+    console.log(e.target.value);
   };
 
   const handleSubmit = (event) => {
-    dispatch(createUser());
+    // dispatch(createUser());
     event.preventDefault();
+    const useDataRegistration = {
+      user: {
+        email: userEmail,
+        password: userPassword,
+        password_confirmation: userPasswordConfirmation,
+      },
+    };
+    registerUser(useDataRegistration);
+    setUserEmail('');
+    setUserPassword('');
+    setUserPasswordCofirmation('');
   };
 
   return (
@@ -38,8 +71,9 @@ const Registration = (props) => {
             type="email"
             name="email"
             placeholder="Email"
-            value={email}
-            onChange={handleChange}
+            // value={email}
+            value={userEmail}
+            onChange={emailHandleChange}
             required
           />
           <Form.Text className="text-muted">
@@ -53,8 +87,9 @@ const Registration = (props) => {
             type="password"
             name="password"
             placeholder="Password"
-            value={password}
-            onChange={handleChange}
+            // value={password}
+            value={userPassword}
+            onChange={passwordHandleChange}
             required
           />
         </Form.Group>
@@ -64,13 +99,16 @@ const Registration = (props) => {
             type="password"
             name="password_confirmation"
             placeholder="Confirm Password"
-            value={password_confirmation}
-            onChange={handleChange}
+            // value={password_confirmation}
+            value={userPasswordConfirmation}
+            onChange={passwordConfirmationHandleChange}
             required
           />
         </Form.Group>
-        <Button type="submit" disabled={isRegistering}>
-          {isRegistering ? 'Registering...' : 'Register'}
+        <Button type="submit">
+          {/* <Button type="submit" disabled={isRegistering}> */}
+          {/* {isRegistering ? 'Registering...' : 'Register'} */}
+          Register
         </Button>
       </Form>
     </div>
