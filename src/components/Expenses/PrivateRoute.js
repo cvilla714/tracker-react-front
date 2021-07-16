95039a2e1d018403fd3bed34fa5fe7d141988794
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-16 02:03:29 -0400

    fixed bugged on rendering the content of the correct component

--- src/components/Expenses/PrivateRoute.js
+++ src/components/Expenses/PrivateRoute.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+// import { useGetLoginUserInfoQuery } from '../../features/user/statusSlice';
 import Home from './Home';
 
 const PrivateRoute = ({ isAuth, component: Component, ...rest }) => {
+  // const { data } = useGetLoginUserInfoQuery();
+  // const isAuth = data?.logged_in ? true : false;
   return (
     <Route
       {...rest}
-      render={(props) => {
+      render={(props) =>
         isAuth ? (
           <Home />
         ) : (
           <Redirect
             to={{ pathname: '/login', state: { from: props.location } }}
           />
-        );
-      }}
+        )
+      }
     />
   );
 };
