a46db1042760dcdae5b61885f6cfd0bbc3cbcb13
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-16 12:36:59 -0400

    imported custom hook to collect value from the form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,46 +2,59 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 import { usePostUserExpensesMutation } from '../../features/user/statusSlice';
 import { useGetLoginUserInfoQuery } from '../../features/user/statusSlice';
+import useForm from '../Hooks/useForm';
 
 const ExpenseForm = (props) => {
-  const [enterTitle, setEnterTitle] = useState('');
-  const [enterAmount, setEnterAmount] = useState('');
-  const [enterDate, setEnterDate] = useState('');
+  // const [enterTitle, setEnterTitle] = useState('');
+  // const [enterAmount, setEnterAmount] = useState('');
+  // const [enterDate, setEnterDate] = useState('');
+
+  // console.log(props);
+
+  const { form, handleChange, clearForm } = useForm({
+    title: '',
+    amount: '',
+    date: '',
+  });
 
   const [postUserExpenses] = usePostUserExpensesMutation();
   const { data: userid } = useGetLoginUserInfoQuery();
 
-  const titleChangeHandler = (e) => {
-    setEnterTitle(e.target.value);
-    console.log(e.target.value);
-  };
+  // const titleChangeHandler = (e) => {
+  //   setEnterTitle(e.target.value);
+  //   console.log(e.target.value);
+  // };
 
-  const amountHandler = (e) => {
-    setEnterAmount(e.target.value);
-    console.log(e.target.value);
-  };
+  // const amountHandler = (e) => {
+  //   setEnterAmount(e.target.value);
+  //   console.log(e.target.value);
+  // };
 
-  const datetHandler = (e) => {
-    setEnterDate(e.target.value);
-    console.log(e.target.value);
-  };
+  // const datetHandler = (e) => {
+  //   setEnterDate(e.target.value);
+  //   console.log(e.target.value);
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const expenseData = {
-      title: enterTitle,
-      amount: +enterAmount,
-      date: new Date(enterDate).toLocaleDateString(),
+      // title: enterTitle,
+      // amount: +enterAmount,
+      // date: new Date(enterDate).toLocaleDateString(),
       // user_id: data.user.id,
       user_id: userid.user.id,
       // date: new Date(enterDate),
+      title: form.title,
+      amount: form.amount,
+      date: new Date(form.date).toLocaleDateString(),
     };
 
     postUserExpenses(expenseData);
+    clearForm();
     // props.onSaveExpenseData(expenseData);
-    setEnterTitle('');
-    setEnterAmount('');
-    setEnterDate('');
+    // setEnterTitle('');
+    // setEnterAmount('');
+    // setEnterDate('');
   };
 
   return (
@@ -51,9 +64,12 @@ const ExpenseForm = (props) => {
           <label htmlFor="">Title</label>
           <input
             type="text"
-            name="enterTitle"
-            value={enterTitle}
-            onChange={titleChangeHandler}
+            name="title"
+            value={form.title}
+            onChange={handleChange}
+            // name="enterTitle"
+            // value={enterTitle}
+            // onChange={titleChangeHandler}
             required
           />
         </div>
@@ -63,9 +79,12 @@ const ExpenseForm = (props) => {
             type="number"
             min="0.01"
             step="0.01"
-            name="enterAmount"
-            value={enterAmount}
-            onChange={amountHandler}
+            name="amount"
+            value={form.amount}
+            onChange={handleChange}
+            // name="enterAmount"
+            // value={enterAmount}
+            // onChange={amountHandler}
             required
           />
         </div>
@@ -75,9 +94,12 @@ const ExpenseForm = (props) => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
-            name="enterDate"
-            value={enterDate}
-            onChange={datetHandler}
+            name="date"
+            value={form.date}
+            onChange={handleChange}
+            // name="enterDate"
+            // value={enterDate}
+            // onChange={datetHandler}
             required
           />
         </div>
