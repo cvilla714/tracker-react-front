82f32110ac39b786213ca0247e3a4fc82d01bc40
Cosmel Villalobos <cosmelvg@gmail.com>
2021-07-23 11:44:24 -0400

    deleted function that were not been used

--- src/components/Chart/BarChart.js
+++ src/components/Chart/BarChart.js
@@ -25,36 +25,14 @@ const BarChart = (props) => {
 
   console.log(filterItByUserId);
 
-  // const filterItByMonth = filterIt
-  //   .filter((vaso) => {
-  //     return (
-  //       new Date(vaso.date).toLocaleString('en-US', { year: 'numeric' }) ===
-  //       props.filterYear
-  //     );
-  //   })
-  //   .map((spoon) =>
-  //     new Date(spoon.date).toLocaleString('en-US', { month: 'long' }),
-  //   );
-
-  // console.log(filterItByMonth);
-
-  // const filterItByMonth = filterIt.filter((vaso) => {
-  //   return (
-  //     new Date(vaso.date).toLocaleString('en-US', { year: 'numeric' }) ===
-  //     props.filterYear
-  //   );
-  // });
-
-  // console.log(filterItByMonth);
   const expensesPerMoth = () => {
-    console.log(filterItByUserId);
     let expenses = {};
 
     filterItByUserId.map((item) => {
       const formatMonth = new Date(item.date).toLocaleString('en-US', {
         month: 'long',
       });
-      // console.log(formatMonth);
+
       expenses[formatMonth]
         ? (expenses[formatMonth] += item.amount)
         : (expenses[formatMonth] = item.amount);
@@ -91,62 +69,7 @@ const BarChart = (props) => {
     );
   };
 
-  // const meses = filterIt.map((masa) => {
-  //   return new Date(masa.date).toLocaleString('en-US', { month: 'long' });
-  // });
-  // console.log(meses);
-
-  // console.log(
-  //   data
-  //     ? data.map((month) =>
-  //         new Date(month.date).toLocaleString('en-US', { month: 'long' }),
-  //       )
-  //     : [],
-  // );
-
-  //   console.log(
-  //     data.map((month) =>
-  //       new Date(month.date).toLocaleString('en-US', { month: 'long' }),
-  //     ),
-  //   );
-  const months = data
-    ? data.map((month) =>
-        new Date(month.date).toLocaleString('en-US', { month: 'long' }),
-      )
-    : [];
-  // console.log(months);
-
-  // const totalExpenses = data ? data.map((expense) => expense.amount) : [];
-  // console.log(totalExpenses);
-
-  // const totalFilterExpenses = filterItByUserId.map((gastos) => gastos.amount);
-  // console.log(totalFilterExpenses);
-
-  // const result = data
-  //   ? data.reduce((acc, total) => {
-  //       return { ...acc, [months]: (acc[total.amount] || 0) + 1 };
-  //     }, {})
-  //   : [];
-
-  // console.log(result);
-
-  return (
-    <div>
-      {/* <Bar
-        data={{
-          // labels: filterItByMonth,
-          datasets: [
-            {
-              label: 'Total Expenses Per Month',
-              // data: totalFilterExpenses,
-              backgroundColor: 'white',
-            },
-          ],
-        }}
-      /> */}
-      {renderValues()}
-    </div>
-  );
+  return <div>{renderValues()}</div>;
 };
 
 export default BarChart;
