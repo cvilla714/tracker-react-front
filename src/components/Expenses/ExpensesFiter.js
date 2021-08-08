fd5c8256d403ef10ce9bb4a67383a1c40a1a5a1d
Cosmel Villalobos <cosmelvg@gmail.com>
2021-08-08 17:15:05 -0400

    Added PropTypes for ExpensesFilter onChangeFilter to be a required function and selected to be elementType both required

--- src/components/Expenses/ExpensesFiter.js
+++ src/components/Expenses/ExpensesFiter.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import './ExpenseFilter.css';
+import PropTypes from 'prop-types';
 
-const ExpensesFiter = (props) => {
+const ExpensesFiter = ({ selected, onChangeFitler }) => {
   const handleClick = (e) => {
-    console.log(e.target.value);
-    props.onChangeFitler(e.target.value);
+    onChangeFitler(e.target.value);
   };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
-        <label>Filter by year</label>
-        <select value={props.selected} onChange={handleClick}>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
-        </select>
+        <label htmlFor={selected}>
+          Filter by year
+          <br />
+          <select value={selected} onChange={handleClick}>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+            <option value="2019">2019</option>
+          </select>
+        </label>
       </div>
     </div>
   );
+};
+
+ExpensesFiter.propTypes = {
+  onChangeFitler: PropTypes.func.isRequired,
+  selected: PropTypes.elementType.isRequired,
 };
 
 export default ExpensesFiter;
