0e3238852b73cfab78dccd114481636990092bda
Cosmel Villalobos <cosmelvg@gmail.com>
2021-08-08 17:10:28 -0400

    Deleted unessary content, and commented code

--- src/app/store.js
+++ src/app/store.js
@@ -1,18 +1,18 @@
+/* eslint-disable max-len */
 import { configureStore } from '@reduxjs/toolkit';
 
 import registrationReducer from '../features/user/registrationSlice';
 import loginReducer from '../features/user/loginSlice';
 import { checkLoginStatusapi } from '../features/user/statusSlice';
-// import usersReducer from '../features/user/statusSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     registration: registrationReducer,
     login: loginReducer,
-    // users: usersReducer,
+
     [checkLoginStatusapi.reducerPath]: checkLoginStatusapi.reducer,
   },
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(checkLoginStatusapi.middleware);
-  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(checkLoginStatusapi.middleware),
 });
+
+export default store;
