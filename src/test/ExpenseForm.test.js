ab7a2ec02bea460f1efe67efb705ce8e3c389470
Cosmel Villalobos <cosmelvg@gmail.com>
2021-08-10 02:13:37 -0400

    Added test for ExpenseForm, checking that Title,Amount and Date are present

---
+++ src/test/ExpenseForm.test.js
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import ExpenseForm from '../components/NewExpense/ExpenseForm';
+import { Provider } from 'react-redux';
+import store from '../app/store';
+
+describe('ExpenseForm Component', () => {
+  test('renders Title', () => {
+    render(
+      <Provider store={store}>
+        <ExpenseForm />
+      </Provider>,
+    );
+
+    const title = screen.getByText('Title');
+    expect(title).toBeInTheDocument();
+  });
+
+  test('renders Amount', () => {
+    render(
+      <Provider store={store}>
+        <ExpenseForm />
+      </Provider>,
+    );
+
+    const amount = screen.getByText('Amount');
+    expect(amount).toBeInTheDocument();
+  });
+
+  test('renders Date', () => {
+    render(
+      <Provider store={store}>
+        <ExpenseForm />
+      </Provider>,
+    );
+
+    const date = screen.getByText('Date');
+    expect(date).toBeInTheDocument();
+  });
+});
