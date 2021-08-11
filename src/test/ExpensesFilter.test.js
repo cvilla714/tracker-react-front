d56d2f02e4dc62b189933c641e9c0a4a517931c6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-08-11 00:44:10 -0400

    corrected component name

--- src/test/ExpensesFilter.test.js
+++ src/test/ExpensesFilter.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import ExpensesFiter from '../components/Expenses/ExpensesFiter';
+import ExpensesFilter from '../components/Expenses/ExpensesFiter';
 
 describe('Expenses Filter Component', () => {
   test('renders Filter By Year', () => {
-    render(<ExpensesFiter />);
+    render(<ExpensesFilter />);
 
     const filter = screen.getByText('Filter by year');
     expect(filter).toBeInTheDocument();
