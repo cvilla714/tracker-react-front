4b2abbadc7afe72b71abe1c461f68bb9d6c51bf2
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-01 01:17:32 -0400

    changed baseUrl to use heroku app name for restfull API

--- src/features/user/statusSlice.js
+++ src/features/user/statusSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const checkLoginStatusapi = createApi({
   reducerPath: 'checkLoginStatus',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'https://lavista-authentication-api.herokuapp.com/',
     credentials: 'include',
   }),
   tagTypes: ['Session', 'Expenses'],
