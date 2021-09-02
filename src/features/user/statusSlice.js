0b3dc4c0afc627b479620d79b80d483820c39f68
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-01 23:33:08 -0400

    Fixed typo in baseUrl

--- src/features/user/statusSlice.js
+++ src/features/user/statusSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const checkLoginStatusapi = createApi({
   reducerPath: 'checkLoginStatus',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://lavista-authentication-api.herokuapp.com/',
+    baseUrl: 'https://lavista-authentication-api.herokuapp.com',
     credentials: 'include',
   }),
   tagTypes: ['Session', 'Expenses'],
