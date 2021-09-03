dfe2e46a0489402b73c4a820dab928c22d12a219
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-03 18:43:28 -0400

    adding mode cors to allow request from front end to the back end

--- src/features/user/statusSlice.js
+++ src/features/user/statusSlice.js
@@ -3,8 +3,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const checkLoginStatusapi = createApi({
   reducerPath: 'checkLoginStatus',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://lavista-authentication-api.herokuapp.com',
+    // baseUrl: 'https://lavista-authentication-api.herokuapp.com',
+    baseUrl: 'http://localhost:3001',
     credentials: 'include',
+    mode: 'cors',
   }),
   tagTypes: ['Session', 'Expenses'],
   endpoints: (builder) => ({
