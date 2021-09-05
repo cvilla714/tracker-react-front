8e33a2f83b6efc0e7dce04ebf80dd9f7eeba2615
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-04 22:42:14 -0400

    deleted mode cors

--- src/features/user/statusSlice.js
+++ src/features/user/statusSlice.js
@@ -4,9 +4,7 @@ export const checkLoginStatusapi = createApi({
   reducerPath: 'checkLoginStatus',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://lavista-authentication-api.herokuapp.com',
-    // baseUrl: 'http://localhost:3001',
     credentials: 'include',
-    mode: 'cors',
   }),
   tagTypes: ['Session', 'Expenses'],
   endpoints: (builder) => ({
